5dae811e20d87cd7656dbce719e55997643769a1
Alex Maimescu <maimescu.alex@gmail.com>
2016-03-02 15:25:57 +0000

    Move files all public headers import in a separated umbrella header.

---
+++ Libs/PublicHeaders.h
@@ -0,0 +1,22 @@
+//
+//  PublicHeaders.h
+//  Insider
+//
+//  Created by Alexandru Maimescu on 3/2/16.
+//  Copyright © 2016 Alex Maimescu. All rights reserved.
+//
+
+#ifndef PublicHeaders_h
+#define PublicHeaders_h
+
+// GCDWebServer https://github.com/swisspol/GCDWebServer
+
+#import <Insider/GCDWebServer.h>
+#import <Insider/GCDWebServerURLEncodedFormRequest.h>
+#import <Insider/GCDWebServerDataResponse.h>
+
+// iOS-System-Services https://github.com/Shmoopi/iOS-System-Services
+
+#import <Insider/SystemServices.h>
+
+#endif /* PublicHeaders_h */
